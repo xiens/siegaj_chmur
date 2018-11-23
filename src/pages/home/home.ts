f51908192b9ec7deb1e7b1a6e9a40667e8855144
necklace <xiens2@gmail.com>
2018-11-23 16:20:05 +0100

    Update home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,7 +12,7 @@ import { StatisticsPage } from '../statistics/statistics';
 export class HomePage {
   
   statisticsPage = StatisticsPage;
-
+  
   url: string;
   urlContent: string; //for counting tags
   listOfUrls;
@@ -35,7 +35,7 @@ export class HomePage {
   }
 
   ionViewDidLoad(){
-    var x;
+	var pd;
     this.findGarbageInfo();
   }
 
